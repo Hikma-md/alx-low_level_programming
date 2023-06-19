b57ab5b8a30dd27e8f17ab35658aaba2689ddda7
hikma mohammed <hikmamohammed1166@gmail.com>
2023-06-19 16:23:08 -0700

    c variables

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints nos of base 16
+ *
+ * Return : 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 8; i < 18; i++)
+	{
+		putchar(i);
+	}
+	for (i = 3; i < 6; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
