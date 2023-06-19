75c871267d1c5cc5656712fd868aa54b76455301
hikma mohammed <hikmamohammed1166@gmail.com>
2023-06-19 14:45:35 -0700

    c variables

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * main - printts lower case alphabet with out q and e
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+	int j;
+	for (j = 1; j < 26; j++)
+	{
+		if (j != 4 && j != 16)
+		{
+			putchar(alpha[j]);
+		}
+	}
+	putchar{'\n'};
+	return (0);
+}
