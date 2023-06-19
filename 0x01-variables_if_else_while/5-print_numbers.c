22a6b5ce9dbb4618123007cb51789402ab8ead86
hikma mohammed <hikmamohammed1166@gmail.com>
2023-06-19 15:18:22 -0700

    c variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints base 10 number
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10: i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\');
+	return (0);
+}
+
