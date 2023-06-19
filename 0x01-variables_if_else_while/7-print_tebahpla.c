63e531e8c28d117d85308344e5d17d378e010599
hikma mohammed <hikmamohammed1166@gmail.com>
2023-06-19 16:08:41 -0700

    c variables

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+char alpha[26] = "zyxwvutsrqponmlkjihgfedcba";
 	int i;
 	
-	for (i = 0; i < 26; i--)
+	for (i = 0; i < 26; i++)
 	{
 		putchar(alpha[i]);
 	}
