98b78b448917a7b660a80b661904f74928616285
hikma mohammed <hikmamohammed1166@gmail.com>
2023-06-19 15:30:25 -0700

    c variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - pprints single digit
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
