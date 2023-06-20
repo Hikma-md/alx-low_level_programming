62721be2723da87ceee1ac497930a1572de3ee2d
hikma mohammed <hikmamohammed1166@gmail.com>
2023-06-20 12:45:12 -0700

    c function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
  * main - prints all lowercase alphabet
  */
+
 void print_alphabet(void)
 {
-	char letter ;
+	char letter;
 	
 	for (letter = 'a'; letter <= 'z'; letter++);
 		_putchar(letter);
