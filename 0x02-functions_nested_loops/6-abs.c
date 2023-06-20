f243f63213225ad94ad05f5c63e4d554ae3a2fbc
hikma mohammed <hikmamohammed1166@gmail.com>
2023-06-20 14:56:02 -0700

    c function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#iclude "main.h"
+
+/**
+ * _abs _ fun compute absolute value of int
+ *
+ * @n: takes in int type
+ *
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
