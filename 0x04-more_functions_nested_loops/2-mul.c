f5701a066c0f5e0de3b7bc9ecf08815f05f4cb9f
hikma mohammed <hikmamohammed1166@gmail.com>
2023-06-22 13:54:43 -0700

    more function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplication
+ * @a: variable for first no
+ * @b: variable for second no 
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+int result = a ^ b;	
+return (result);
+}
