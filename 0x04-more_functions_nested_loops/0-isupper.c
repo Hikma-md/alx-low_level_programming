78f9439593c277c334d0ea7412474d7fea7e5be3
hikma mohammed <hikmamohammed1166@gmail.com>
2023-06-22 13:21:57 -0700

    more function

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - prints capital and small letters
+ *@c: the charactre to be cheked
+ * 
+ * Return: 0, 1
+ */
+
+int _isupper(int c)
+
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+return (0);
+}
