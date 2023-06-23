2bdbf596e963d19aa283bab7703083d681ac3bd6
hikma mohammed <hikmamohammed1166@gmail.com>
2023-06-23 14:04:58 -0700

    b

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,14 @@
  */
 void print_numbers(void)
 {
-	char c;
+int i;
 
-	for (c = '0'; c <= 9; c++)
-	{
-	_putchar(c);
-	}
-	_putchar('\n');
+i=0;
+while (i < 10)
+{
+_putchar(i + '0');
+i++;
 }
+_putchar('\n');
+}
+
--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -4,7 +4,7 @@
  * print character
  * Return: 2
  */
-int _putchar(char c)
+int _putchar(int char c)
 {
-        return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
